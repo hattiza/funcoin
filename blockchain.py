3b48f10a4a22dd109554a6f007e0b3a946ff75c7
hattiza <quiet.iron8712@fastmail.com>
2024-06-16 15:33:55 -0700

    Add blockchain stub

---
+++ blockchain.py
@@ -0,0 +1,13 @@
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+
+    def new_block(self):
+        pass
+
+    @staticmethod
+    def hash(block):
+        pass
+
+    def last_block(self):
+        pass
